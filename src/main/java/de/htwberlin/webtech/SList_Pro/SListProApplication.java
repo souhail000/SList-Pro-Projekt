aefefb8cb492fcc1c77fbbbda5c75c44a48a7524
souhail000 <Souhail.belkheir@Student.HTW-Berlin.de>
2026-04-19 00:23:23 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/SList_Pro/SListProApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.SList_Pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SListProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SListProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/SList_Pro/SListProApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.SList_Pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SListProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
